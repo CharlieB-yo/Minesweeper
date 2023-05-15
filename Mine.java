dc43dbb8d097b01f0278d6f8598afdbdbd53eada
CharlieB-yo <111457383+CharlieB-yo@users.noreply.github.com>
2023-05-15 12:18:06 -0700

    Create Mine.java

---
+++ Mine.java
@@ -0,0 +1,10 @@
+public class Mine{
+    public boolean bomb;
+    public boolean flagged;
+    public boolean clicked;
+    public Mine(boolean b){
+        this.bomb = b;
+        this.flagged = false;
+        this.clicked = false;
+    }
+}
